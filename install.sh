0107286f245d0b11ef53a61e2e14d2b140d40529
Domink Sigmund <dominik.sigmund@br.de>
2016-07-15 08:45:59 +0200

    Added installation of curl to install-skript

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 echo 'Verbshaker Installer started'
 echo 'Installing System Requirements'
+sudo apt-get install -y curl
 curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
 sudo apt-get update
 sudo apt-get install -y nodejs git build-essential
